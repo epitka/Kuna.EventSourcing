a9f7fae46d69ad6cf0b837225e44e887b109c45f
Edvard Pitka <epitka014@gmail.com>
2022-12-20 10:09:26 +0100

    add_ICommandDispatcher (#23)
    added ICommandDispatcher

--- Core/src/Commands/ICommandBus.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Senf.EventSourcing.Core.Commands;
-
-public interface ICommandBus
-{
-    Task Send<TCommand>(TCommand command) where TCommand : ICommand;
-}
---
+++ Core/src/Commands/ICommandDispatcher.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Senf.EventSourcing.Core.Commands;
+
+public interface ICommandDispatcher
+{
+    Task Send<TCommand>(TCommand command, CancellationToken ct)
+        where TCommand : class, ICommand;
+}
+
+public class CommandDispatcher : ICommandDispatcher
+{
+    private readonly IServiceProvider serviceProvider;
+
+    public CommandDispatcher(IServiceProvider serviceProvider)
+    {
+        this.serviceProvider = serviceProvider;
+    }
+
+    public async Task Send<TCommand>(TCommand command, CancellationToken ct)
+        where TCommand : class, ICommand
+    {
+        var handler = this.serviceProvider.GetRequiredService<IHandleCommand<TCommand>>();
+
+        await handler.Handle(command, ct)
+                     .ConfigureAwait(false);
+    }
+}
