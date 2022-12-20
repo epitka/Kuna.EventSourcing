4a2f2b748c2fb11550ace8c2d4fd94eb5ea3799a
epitka <epitka014@gmail.com>
2022-12-20 11:55:38 +0100

    added ContainerDrivenTest helper

---
+++ Core/src/Configuration/IServiceConfigurator.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Senf.EventSourcing.Core.Configuration;
+
+public interface IServicesConfigurator
+{
+    void ConfigureServices(IServiceCollection services);
+}
