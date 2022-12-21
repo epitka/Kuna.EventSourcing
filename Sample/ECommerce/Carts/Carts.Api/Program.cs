e4f6093141afa823c1d5bdde0c22cc607da37e35
Edvard Pitka <epitka014@gmail.com>
2022-12-21 16:29:17 +0100

    example_3 (#27)
    added API project

--- Sample/ECommerce/Carts/Carts.Api/Program.cs
+++ Sample/ECommerce/Carts/Carts.Api/Program.cs
@@ -5,45 +5,42 @@ using Microsoft.OpenApi.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services
-    .AddSwaggerGen(c =>
-    {
-        c.SwaggerDoc("v1", new OpenApiInfo { Title = "Carts", Version = "v1" });
-       // c.OperationFilter<MetadataOperationFilter>();
-    })
-    //.AddKafkaProducer()
-    //.AddCoreServices()
-    //.AddCartsModule(builder.Configuration)
-    //.AddCorrelationIdMiddleware()
-    /*.AddOptimisticConcurrencyMiddleware(
-        sp => sp.GetRequiredService<MartenExpectedStreamVersionProvider>().TrySet,
-        sp => () => sp.GetRequiredService<MartenNextStreamVersionProvider>().Value?.ToString()
-    )*/
-    .AddControllers()
-    // .AddNewtonsoftJson(opt => opt.SerializerSettings.WithDefaults())
-    ;
+       .AddSwaggerGen(c =>
+       {
+           c.SwaggerDoc("v1", new OpenApiInfo { Title = "Carts", Version = "v1" });
+          // c.OperationFilter<MetadataOperationFilter>();
+       })
+
+      /*.AddCorrelationIdMiddleware()
+       .AddOptimisticConcurrencyMiddleware(
+           sp => sp.GetRequiredService<EventStoreDBExpectedStreamRevisionProvider>().TrySet,
+           sp => () => sp.GetRequiredService<EventStoreDBNextStreamRevisionProvider>().Value?.ToString()
+       )*/
+       .AddControllers();
 
 var app = builder.Build();
 
-/*app.UseExceptionHandlingMiddleware(exception => exception switch
-    {
-        AggregateNotFoundException _ => HttpStatusCode.NotFound,
-        ConcurrencyException => HttpStatusCode.PreconditionFailed,
-        _ => HttpStatusCode.InternalServerError
-    })
-    .UseCorrelationIdMiddleware()
-    .UseOptimisticConcurrencyMiddleware()
-    .UseRouting()
-    .UseAuthorization()
-    .UseEndpoints(endpoints =>
-    {
-        endpoints.MapControllers();
-    })
-    .UseSwagger()
-    .UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Carts V1");
-        c.RoutePrefix = string.Empty;
-    });*/
+app
+    /*.UseExceptionHandlingMiddleware(exception => exception switch
+   {
+       AggregateNotFoundException _  => HttpStatusCode.NotFound,
+       WrongExpectedVersionException => HttpStatusCode.PreconditionFailed,
+       _                             => HttpStatusCode.InternalServerError
+   })
+   .UseCorrelationIdMiddleware()
+   .UseOptimisticConcurrencyMiddleware()*/
+   .UseRouting()
+   .UseAuthorization()
+   .UseEndpoints(endpoints =>
+   {
+       endpoints.MapControllers();
+   })
+   .UseSwagger()
+   .UseSwaggerUI(c =>
+   {
+       c.SwaggerEndpoint("/swagger/v1/swagger.json", "Carts V1");
+       c.RoutePrefix = string.Empty;
+   });
 
 app.Run();
 
--- Sample/ECommerce/Carts/Carts.Domain/Commands/CancelShoppingCart.cs
+++ Sample/ECommerce/Carts/Carts.Domain/Commands/CancelShoppingCart.cs
@@ -1,3 +1,5 @@
+
+
 namespace Carts.Domain.Commands;
 
 public sealed record CancelShoppingCart(GuidId CartId)
