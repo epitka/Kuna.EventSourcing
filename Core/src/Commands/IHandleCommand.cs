90abe393d76aee4ae919d0b629634161226d40b2
Edvard Pitka <epitka014@gmail.com>
2023-03-04 08:53:04 +0100

    added support for commands that return (#34)

--- Core/src/Commands/IHandleCommand.cs
+++ Core/src/Commands/IHandleCommand.cs
@@ -1,11 +1,19 @@
-﻿namespace Kuna.EventSourcing.Core.Commands;
+﻿using System.Reflection.Metadata;
+
+namespace Kuna.EventSourcing.Core.Commands;
 
 /// <summary>
 /// Marker interface used to identify handlers of commands
 /// </summary>
 /// <typeparam name="T"></typeparam>
 public interface IHandleCommand<in TCommand>
-    where TCommand : class, ICommand
+    where TCommand : ICommand
 {
     Task Handle(TCommand command, CancellationToken ct);
+}
+
+public interface IHandleCommand<in TCommand, TReturn>
+    where TCommand : ICommand<TReturn>
+{
+    Task<TReturn> Handle(TCommand command, CancellationToken ct);
 }
